2578257d99d9ee0e09d85e6230f70cbeacf35a09
Irfan-debug538 <Irfan.Basha.Shaik@ibm.com>
2024-12-19 10:37:37 +0530

    Add files via upload

---
+++ Java/Activities/Activity2.java
@@ -0,0 +1,16 @@
+
+public class Activity2 {
+	public static void main(String[] args) {
+		int[] numbers = {10,77,10,54,-11,10};
+		
+		int sum = 0;
+		for (int num: numbers) {
+			if (num == 10) {
+				sum += num;
+				
+			}
+		}
+		System.out.println(sum);
+	}
+
+}
