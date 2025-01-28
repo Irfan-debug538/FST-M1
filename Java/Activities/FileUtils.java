7c5b8f102c36af52aeb207b85a9b37e29505e5bc
Irfan-debug538 <Irfan.Basha.Shaik@ibm.com>
2025-01-28 19:04:05 +0530

    Add files via upload

--- Java/Activities/FileUtils.java
+++ Java/Activities/FileUtils.java
@@ -1,42 +1,27 @@
 package org.apache.commons.io;
 
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
 
 public class FileUtils {
 
-	public static String readFileToString(File fileUtil, String string) {
-		String newData = string + " is added from method";
-		//return data from the file newFile.txt
-		return newData;
-		
+	public static File getFile(String string) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
-	public static String getFile(File destDir, String string) {
-		destDir.toString();
-		String newPath = destDir + "\\" + string;
-		return newPath;
+	public static String readFileToString(File fileUtil, String string) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 	public static void copyFileToDirectory(File file, File destDir) {
-		Path source = file.toPath(); 
-        Path destination = destDir.toPath();   
-		try {  
-            Files.copy(source, destination, StandardCopyOption.REPLACE_EXISTING);  
-            System.out.println("File copied successfully using traditional I/O.");  
-        } catch (IOException e) {  
-            e.printStackTrace();  
-        }  
+		// TODO Auto-generated method stub
 		
 	}
 
-	public static String getFile(String str) throws IOException {
-		Path file = Path.of(str);
-		String dataOfFile = Files.readString(file);
-		return dataOfFile;
+	public static File getFile(File destDir, String string) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
